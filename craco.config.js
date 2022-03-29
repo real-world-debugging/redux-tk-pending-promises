0a1614b7b054b6117453fdbfce13f64eff7b9d6c
Domiii <Domiii@users.noreply.github.com>
2022-03-30 05:03:14 +0800

    figuring out some config things

--- craco.config.js
+++ craco.config.js
@@ -2,14 +2,21 @@
 
 const { getLoaders, loaderByName } = require('@craco/craco');
 
+const { inspect } = require('util');
+
+// const DbuxEnabled = false;
+const DbuxEnabled = true;
+
 const dbuxOptions = {
   moduleFilter: {
     packageWhitelist: '.*redux.*',
     // packageWhitelist: '.*', // NOTE: quite slow
 
     /**
-     * NOTE: we blacklist some of these since they mostly muddy the search space.
-     * 1. Some of these are polyfills (probably brought in by CRA) and their libraries (loaded when first requiring things, and before `@dbux/runtime`?)
+     * NOTEs:
+     * 1. we blacklist some of these since they mostly muddy the search space.
+     * 2. Some of these are polyfills (probably brought in by CRA) and their libraries (loaded when first requiring things, and before `@dbux/runtime`?)
+     * 3. react-error-overlay brings in `async-generator-runtime`, and it does not like to be touched
      */
     packageBlacklist: 'react-dev-utils,react-refresh,react-error-overlay,process,buffer,isarray,ieee754,base64-js',
     fileWhitelist: '.*',
@@ -19,50 +26,60 @@ const dbuxOptions = {
 };
 
 module.exports = {
+  devServer: {
+    writeToDisk: true
+  },
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
-      const { hasFoundAny, matches } = getLoaders(webpackConfig, loaderByName("babel-loader"));
+      if (DbuxEnabled) {
+        const { hasFoundAny, matches } = getLoaders(webpackConfig, loaderByName("babel-loader"));
 
 
-      // // TODO: write output files (this won't work, `devServer` does not exit)
-      // webpackConfig.devServer.devMiddleware.writeToDisk = true;
+        // // TODO: write output files (this won't work, `devServer` does not exit)
+        // webpackConfig.devServer.devMiddleware.writeToDisk = true;
 
-      // // exclude dbux from all rules
-      // const reported = new Set();
-      // webpackConfig.module.rules.forEach(rule => {
-      //   if (rule.exclude && !Array.isArray(rule.exclude)) {
-      //     rule.exclude = [rule.exclude];
-      //   }
-      //   if (!rule.exclude) {
-      //     rule.exclude = [];
-      //   }
-      //   rule.exclude.push((modulePath) => {
-      //     const exclude = modulePath.includes('@dbux');
-      //     if (!reported.has(modulePath)) {
-      //       reported.add(modulePath);
-      //       console.debug(`[WEBPACK]`, modulePath, !exclude);
-      //     }
-      //     return exclude;
-      //   });
-      // });
+        // // exclude dbux from all rules
+        // const reported = new Set();
+        // webpackConfig.module.rules.forEach(rule => {
+        //   if (rule.exclude && !Array.isArray(rule.exclude)) {
+        //     rule.exclude = [rule.exclude];
+        //   }
+        //   if (!rule.exclude) {
+        //     rule.exclude = [];
+        //   }
+        //   rule.exclude.push((modulePath) => {
+        //     const exclude = modulePath.includes('@dbux');
+        //     if (!reported.has(modulePath)) {
+        //       reported.add(modulePath);
+        //       console.debug(`[WEBPACK]`, modulePath, !exclude);
+        //     }
+        //     return exclude;
+        //   });
+        // });
 
-      // TODO: we might have to remove preset-env and add ts-only, and before Dbux?
+        // TODO: we might have to remove preset-env and add ts-only, and before Dbux?
 
-      // add dbux to loader
-      if (hasFoundAny) {
-        matches.forEach(match => {
-          const babelOptions = match.loader.options;
-          if (!babelOptions.plugins) {
-            babelOptions.plugins = [];
-          }
-          // // NOTE: weird bug with a class having public fields (in @dbux/runtime) that Babel complains about
-          // babelOptions.plugins.push(['@babel/plugin-proposal-class-properties', { loose: true }]);
-          babelOptions.plugins.push(['@dbux/babel-plugin', dbuxOptions]);
-        });
-        console.debug(`Added @dbux/babel-plugin to babel-loaders.`);
-      }
-      else {
-        throw new Error(`Could not inject Dbux: 'babel-loader' found`);
+        // add dbux to loader
+        if (hasFoundAny) {
+          matches.forEach(match => {
+            const babelOptions = match.loader.options;
+
+            // console.log(`[craco] match ${inspect(
+            //   match.loader.options.presets
+            // )}`);
+
+            if (!babelOptions.plugins) {
+              babelOptions.plugins = [];
+            }
+            babelOptions.plugins.push(['@dbux/babel-plugin', dbuxOptions]);
+          });
+          console.debug(`Added @dbux/babel-plugin to babel-loaders.`);
+        }
+        else {
+          throw new Error(`Could not inject Dbux: 'babel-loader' found`);
+        }
+
+        // throw new Error('STOP');
       }
 
       return webpackConfig;
